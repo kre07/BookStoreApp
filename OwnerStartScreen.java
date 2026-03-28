a2b73ad4e358612031e29caf71a6f3eb4525ac01
Kritagya Sharma <152318154+kre07@users.noreply.github.com>
2026-03-28 15:16:25 -0400

    Add files via upload

---
+++ OwnerStartScreen.java
@@ -0,0 +1,51 @@
+//package bookstoreapp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class OwnerStartScreen extends JPanel implements ActionListener {
+    private JButton booksButton;
+    private JButton customerButton;
+    private JButton logoutButton;
+    private BookStoreApp app;
+    private BookStore store;
+
+    public OwnerStartScreen(BookStoreApp app, BookStore store) {
+        this.app = app;
+        this.store = store;
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        // Requirement: The owner-start-screen has three buttons: [Books], [Customers], [Logout] 
+        booksButton = new JButton("Books");
+        customerButton = new JButton("Customers");
+        logoutButton = new JButton("Logout");
+
+        booksButton.addActionListener(this);
+        customerButton.addActionListener(this);
+        logoutButton.addActionListener(this);
+
+        gbc.gridx = 0; gbc.gridy = 0;
+        add(booksButton, gbc);
+        gbc.gridy = 1;
+        add(customerButton, gbc);
+        gbc.gridy = 2;
+        add(logoutButton, gbc);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == booksButton) {
+            app.showPanel(new OwnerBooksScreen(app, store));
+        } else if (e.getSource() == customerButton) {
+            app.showPanel(new OwnerCustomerScreen(app, store));
+        } else if (e.getSource() == logoutButton) {
+            // Requirement: When the owner clicks the button [Logout], she should be taken back to the login-screen [cite: 70]
+            app.showPanel(new LoginScreen(app, store)); 
+        }
+    }
+}
